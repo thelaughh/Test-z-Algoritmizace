53206aed91e74b0e7c42edc269a10a71027ca727
Jan Gergel <jan.gergel@educhem.cz>
2022-03-14 20:19:50 +0100

    Přidejte soubory projektu.

---
+++ TEst na Alp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+List<double> seznam = new List<double>();
+seznam.Add(0.77);
+seznam.Add(150.24);
+seznam.Add(125.98);
+seznam.Add(50.65);
+seznam.Add(53.68);
+foreach (double hodnota in seznam)
+{
+    if (hodnota > 100)
+    {
+        Console.WriteLine(Math.Ceiling(hodnota));
+    }
+}
+
